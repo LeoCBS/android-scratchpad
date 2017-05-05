c1cf4d5f2814be1c2378e11cb96b7072788f0b5f
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2017-05-05 10:33:31 -0300

    add model package

---
+++ app/src/main/java/com/chefcaseiro/model/Menu.java
@@ -0,0 +1,34 @@
+package com.chefcaseiro.model;
+
+
+public class Menu {
+
+    private String uid;
+    private String name;
+
+    public Menu(){
+
+    }
+
+
+    public Menu(String uid, String name) {
+        this.uid = uid;
+        this.name = name;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
